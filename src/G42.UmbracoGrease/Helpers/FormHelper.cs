2b0e4c8beba109ebf88fb0ea5da7674344e8ca19
kgiszewski <kevin@giszewski.com>
2015-09-17 12:46:58 -0400

    Wrap SendEmail in using statement to properly dispose message

--- src/G42.UmbracoGrease/Helpers/FormHelper.cs
+++ src/G42.UmbracoGrease/Helpers/FormHelper.cs
@@ -7,32 +7,34 @@ namespace G42.UmbracoGrease.Helpers
     {
         public static void SendMail(string toCsv, string from, string subject, string body, bool html, string ccCsv = "", string bccCsv = "")
         {
-            var message = new MailMessage()
+            using (var message = new MailMessage()
             {
                 From = new MailAddress(from),
                 Subject = subject,
                 IsBodyHtml = html
-            };
-
-            //to
-            message.To.Add(toCsv);
-
-            //cc
-            if (!string.IsNullOrEmpty(ccCsv))
+            })
             {
-                message.CC.Add(ccCsv);
+
+                //to
+                message.To.Add(toCsv);
+
+                //cc
+                if (!string.IsNullOrEmpty(ccCsv))
+                {
+                    message.CC.Add(ccCsv);
+                }
+
+                //bcc
+                if (!string.IsNullOrEmpty(bccCsv))
+                {
+                    message.Bcc.Add(bccCsv);
+                }
+
+                message.Body = body;
+
+                var smtp = new SmtpClient();
+                smtp.Send(message);
             }
-
-            //bcc
-            if (!string.IsNullOrEmpty(bccCsv))
-            {
-                message.Bcc.Add(bccCsv);
-            }
-
-            message.Body = body;
-
-            var smtp = new SmtpClient();
-            smtp.Send(message);
         }
 
         public static void SendFormEmail(string sendToEmailCsv, string sendFromEmail, string sendToSubject, string formattedForm)
