2e48b2730d0906249068e32c353a5c512a580d07
kgiszewski <kevin@giszewski.com>
2016-04-26 14:01:53 -0400

    Begin tests

---
+++ src/G42.UmbracoGrease.Tests/G42404Tests/FunctionalTests.cs
@@ -0,0 +1,24 @@
+﻿using G42.UmbracoGrease.Core;
+using NUnit.Framework;
+
+namespace G42.UmbracoGrease.Tests.G42404Tests
+{
+    [TestFixture]
+    public class FunctionalTests
+    {
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            PetaPocoUnitOfWork.ConnectionString = "testDb";
+
+            Grease.Services.G42404Service.Create404TrackerTable();
+            Grease.Services.G42404Service.Create404DomainPathsTable();
+        }
+
+        [Test]
+        public void Can_Add_404()
+        {
+            
+        }
+    }
+}
